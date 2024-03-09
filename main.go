12717f0c0c6b94eba7dd338dae22674314b8ef09
Mohs9n <mohsen.alokaisy@gmail.com>
2024-03-09 11:04:14 +0200

    added boltDB

--- main.go
+++ main.go
@@ -1,74 +1,90 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os/exec"
 	"time"
+
+	bolt "go.etcd.io/bbolt"
 )
 
 func main() {
 	now := time.Now()
 
-	// fmt.Println(ttbc.Data[0].Timings.Fajr)
-	// fmt.Printf("%v",ttbc)
-	ttbc := fetchTimings()
+	db, err := bolt.Open("/home/mohsen/src/go/csps/csps.db", 0600, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
 
-	today_prayers := ttbc.Data[now.Day()].Timings
-	const layout = "15:04 (EET)"
-	parsedTime, err := time.Parse(layout, today_prayers.Fajr)
+	var todayTimes [5]time.Time
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		bktName := []byte("times")
+		keyName := []byte(fmt.Sprintf("%v/%v", now.Year(), int(now.Month())))
+		bkt := tx.Bucket(bktName)
+		if bkt == nil {
+			bkt, err = tx.CreateBucket(bktName)
+			if err != nil {
+				return fmt.Errorf("error creating bucket")
+			}
+
+			timings, err := parseTimings()
+			if err != nil {
+				return err
+			}
+			tmjson, err := json.Marshal(timings)
+			if err != nil {
+				return err
+			}
+
+			err = bkt.Put(keyName, tmjson)
+			if err != nil {
+				return err
+			}
+		} 
+    bkt = tx.Bucket(bktName)
+    if bkt == nil {
+      return fmt.Errorf("Bucket not there??")
+    }
+    timingsJson := bkt.Get(keyName)
+    var timings Timings
+    err = json.Unmarshal(timingsJson, &timings)
+    if err != nil {
+      return err
+    }
+    todayTimes = timings.TimeTable[now.Day() -1 ] 
+
+		return nil
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+  sleep(todayTimes)
+}
+
+func sleep(today_times [5]time.Time) {
+	sleepDuration, err := getNextSleep(today_times)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Sleeping for", sleepDuration-(10*time.Minute))
+	time.Sleep(sleepDuration - (10 * time.Minute))
+
+	cmd := exec.Command("notify-send", "-e", "-u", "critical", "10 Minutes to Next Prayer! Computer Sleeping in 5 Minutes!")
+	_, err = cmd.Output()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	defaultDate := time.Date(now.Year(), now.Month(), 8, 0, 0, 0, 0, time.UTC)
+	log.Println("Sleeping for: ", (5 * time.Minute))
+	time.Sleep(5 * time.Minute)
 
-	// Combine the parsed time with the default date
-	combinedTime := time.Date(defaultDate.Year(), defaultDate.Month(), defaultDate.Day(), parsedTime.Hour(), parsedTime.Minute(), 0, 0, time.UTC)
-
-	fmt.Println("Parsed time with default date:", combinedTime)
-
-	// location, err := time.LoadLocation("Africa/Cairo") // Adjust based on actual EET location
-	if err != nil {
-		fmt.Println("Error loading location:", err)
-		return
-	}
-	times_string := make([]string, 5)
-	times_string[0] = today_prayers.Fajr
-	times_string[1] = today_prayers.Dhuhr
-	times_string[2] = today_prayers.Asr
-	times_string[3] = today_prayers.Maghrib
-	times_string[4] = today_prayers.Isha
-
-
-	times := make([]time.Time, 5)
-
-	for _, e := range times_string {
-		ptime, err := time.Parse(layout, e)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		pptime := time.Date(now.Year(), now.Month(), now.Day(), ptime.Hour(), ptime.Minute(), ptime.Second(), ptime.Nanosecond(), now.Location())
-		times = append(times, pptime)
-	}
-
-	cmd := exec.Command("notify-send", "-i", "/home/mohsen/Downloads/dotnet.svg", "-e", "-u", "critical", "hello")
-	out, err := cmd.Output()
+	sleepCmd := exec.Command("systemctl", "suspend")
+	_, err = sleepCmd.Output()
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	outstr := string(out)
-	log.Println(outstr, "Notif")
-
-	before := time.Now()
-	time.Sleep(1 * time.Second)
-	after := time.Now()
-
-	time.Sleep(after.Sub(before))
-	afterafter := time.Now()
-	fmt.Printf("%v\n", afterafter.Sub(before))
-	fmt.Printf("%v\n", combinedTime.Sub(now))
-
-  parseTimings()
 }
